5a0c85dd9c56302bd5af76e0be1cd19ec2832316
Gustavo Melo <gustavomelo13205@gmail.com>
2025-08-25 14:48:59 -0300

    Prática 11 - Parte 1: Configuração das dependências do Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,17 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     alias(libs.plugins.google.gms.google.services)
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
 
 android {
     namespace = "com.weatherapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.weatherapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -27,6 +28,10 @@ android {
         props.load(keyFile.inputStream())
         buildConfigField ("String", "WEATHER_API_KEY",
             props.getProperty("WEATHER_API_KEY"))
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     buildTypes {
@@ -52,6 +57,11 @@ android {
 }
 
 dependencies {
+
+    val room_version = "2.7.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 
     implementation("io.coil-kt:coil-compose:2.5.0") // coil
     implementation("com.squareup.picasso:picasso:2.8") // Picasso
